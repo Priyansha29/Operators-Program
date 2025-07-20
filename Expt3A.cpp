429810fbc55675469558251b42d546b2e1807f6d
Priyansha29 <priyanshagour29@gmail.com>
2025-07-20 19:57:26 +0530

    Create Expt3A.cpp

---
+++ Expt3A.cpp
@@ -0,0 +1,22 @@
+//PRIYANSHA GOUR 
+//24070123079
+//ENTC-A
+// EXPERIMENT - 3
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a;
+    cout<<"enter number : ";
+    cin>>a;
+    if (a>0){
+        cout<<"number is positive "<<endl;}
+    else if (a<0){
+        cout<<"number is negative"<<endl;}
+    else {
+        cout<<"number is zero"<<endl;
+    }
+    
+
+    return 0;
